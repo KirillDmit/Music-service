68b256c854ca29af8a8bd0a0a1016d65798e84eb
Kirill Dmitriev <kirilldmitriev20015@gmail.com>
2025-03-24 00:24:28 +0500

    add Track Service

---
+++ src/main/java/org/example/service/TrackService.java
@@ -0,0 +1,27 @@
+package org.example.service;
+
+import org.example.entity.Track;
+import org.example.repository.TrackRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TrackService {
+
+    private final TrackRepository trackRepository;
+
+    @Autowired
+    public TrackService(TrackRepository trackRepository) {
+        this.trackRepository = trackRepository;
+    }
+
+    public List<Track> getAllTracks() {
+        return trackRepository.findAll();
+    }
+
+    public Track saveTrack(Track track) {
+        return trackRepository.save(track);
+    }
+}
