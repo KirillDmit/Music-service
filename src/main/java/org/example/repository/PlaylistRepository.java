7ae0eca4a36a5a1441d20060134ae37a9322f247
Kirill Dmitriev <kirilldmitriev20015@gmail.com>
2025-04-06 00:05:56 +0500

    add playlist repository

---
+++ src/main/java/org/example/repository/PlaylistRepository.java
@@ -0,0 +1,13 @@
+package org.example.repository;
+
+import org.example.entity.Playlist;
+import org.example.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
+    List<Playlist> findByUser(User user);
+}
