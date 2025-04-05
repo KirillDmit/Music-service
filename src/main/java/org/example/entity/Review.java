cbaabf8ba10f83894c978e8c9afb596b2cd6d33a
Kirill Dmitriev <kirilldmitriev20015@gmail.com>
2025-04-05 23:58:38 +0500

    add review

---
+++ src/main/java/org/example/entity/Review.java
@@ -0,0 +1,29 @@
+package org.example.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "reviews")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "track_id", nullable = false)
+    private Track track;
+
+    @Column(nullable = false)
+    private int rating;
+
+    @Column(length = 1000)
+    private String reviewText;
+}
