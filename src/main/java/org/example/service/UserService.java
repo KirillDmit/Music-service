30335753980882e286b71d0d6638d6479b83c6eb
Kirill Dmitriev <kirilldmitriev20015@gmail.com>
2025-04-06 15:16:11 +0500

    add user service

---
+++ src/main/java/org/example/service/UserService.java
@@ -0,0 +1,23 @@
+package org.example.service;
+
+import org.example.entity.User;
+import org.example.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+}
